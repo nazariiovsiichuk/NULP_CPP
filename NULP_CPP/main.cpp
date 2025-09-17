79a587030393adbf31a82aab4f4f6e51007ec361
nazariiovsiichuk <Nazarii.Ovsiichuk.AP.2024@lpnu.ua>
2025-09-17 09:13:43 +0300

    Initial Commit

---
+++ NULP_CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NULP_CPP
+//
+//  Created by Nazarii Ovsiichuk on 17.09.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
